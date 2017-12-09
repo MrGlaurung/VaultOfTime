73ec96d545a3ea2b223bc1e1f0712f052f16f0bd
Anders Liden <st@liden.cx>
2017-12-09 22:20:39 +0100

    Added examples of some of the off line answers to DB calls.

---
+++ public/Assets/Scripts/VaultOfTimeApp/OfflineDB/People.js
@@ -0,0 +1,66 @@
+/*
+ *  Test of the javascript functionality on the site with off line data.
+ *
+ *  Peoples in the database.
+ *  When this is actually active then it will not list all people of course, but now I will just
+ *  include a whole bunch of people.
+ */
+
+var People =
+    [
+        {
+            PeopleID: 1,
+            Name: "Anders Liden",
+            Email: "al@liden.cx"
+        },
+        {
+            PeopleID: 1,
+            Name: "Anders Liden",
+            Email: "al@liden.cx"
+        },
+        {
+            PeopleID: 2,
+            Name: "Cecilia Liden",
+            Email: "cecilia@liden.cx"
+        },
+        {
+            PeopleID: 3,
+            Name: "Bavo de Ridder",
+            Email: "bderidder@gmail.com"
+        },
+        {
+            PeopleID: 4,
+            Name: "Effin Lars",
+            Email: "effin@liden.cx"
+        },
+        {
+            PeopleID: 5,
+            Name: "Scooby Doo",
+            Email: "scooby@liden.cx"
+        },
+        {
+            PeopleID: 6,
+            Name: "Flughurt Bumstrappen",
+            Email: "yearight@liden.cx"
+        },
+        {
+            PeopleID: 7,
+            Name: "Huggel Buggel",
+            Email: "huggel@liden.cx"
+        },
+        {
+            PeopleID: 8,
+            Name: "Mr Fantastic",
+            Email: "fantastic@liden.cx"
+        },
+        {
+            PeopleID: 9,
+            Name: "Scary Cary",
+            Email: "cary@liden.cx"
+        },
+        {
+            PeopleID: 10,
+            Name: "Murderus Perderus",
+            Email: "perderus@liden.cx"
+        }
+    ];
---
+++ public/Assets/Scripts/VaultOfTimeApp/OfflineDB/Projects.js
@@ -0,0 +1,21 @@
+/*
+ *  Test of the javascript functionality on the site with off line data.
+ *
+ *  Projects is the projects in the database.
+ */
+
+var Projects =
+    [
+        {
+            ProjectID: 1,
+            Name: 'Vault of Time'
+        },
+        {
+            ProjectID: 2,
+            Name: 'Lord of the Flies'
+        },
+        {
+            ProjectID: 3,
+            Name: 'Belgium trips'
+        }
+    ];
